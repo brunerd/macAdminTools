03e0bb5d1900c5e601d30805e598ff449704b544
Joel Bruner <joel@brunerd.com>
2021-09-10 13:17:08 -0500

    Update setAutoLogin.sh
    Fixed padding with string length of multiple 12. Fixed version.

--- Scripts/setAutoLogin.sh
+++ Scripts/setAutoLogin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 : <<-LICENSE_BLOCK
-setAutoLogin (20200910) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
+setAutoLogin (20210910) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
 Licensed under the MIT License
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
