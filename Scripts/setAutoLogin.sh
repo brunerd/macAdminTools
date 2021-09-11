25f264ef5325268b4317aa6534c30d62ff30eb7b
Joel Bruner <joel@brunerd.com>
2021-09-11 00:10:08 -0500

    Update setAutoLogin.sh
    Optimized to encode and pad in same loop

--- Scripts/setAutoLogin.sh
+++ Scripts/setAutoLogin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 : <<-LICENSE_BLOCK
-setAutoLogin (20210910) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
+setAutoLogin (20210911) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
 Licensed under the MIT License
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -29,46 +29,35 @@ function kcpasswordEncode {
 	local thisString="${1}"
 	local i
 
-	#converted to hex representation with spaces
-	local thisStringHex_array=( $(echo -n "${thisString}" | xxd -p -u | sed 's/../& /g') )
-
 	#macOS cipher hex ascii representation array
 	local cipherHex_array=( 7D 89 52 23 D2 BC DD EA A3 B9 1F )
 
-	#cycle through each element of the array
-	for ((i=0; i < ${#thisStringHex_array[@]}; i++)); do
+	#converted to hex representation with spaces
+	local thisStringHex_array=( $(echo -n "${thisString}" | xxd -p -u | sed 's/../& /g') )
+
+	#get padding by subtraction if under 12 
+	if [ "${#thisStringHex_array[@]}" -lt 12  ]; then
+		local padding=$(( 12 -  ${#thisStringHex_array[@]} ))
+	#get padding by subtracting remainder of modulo 12 if over 12 
+	elif [ "$(( ${#thisStringHex_array[@]} % 12 ))" -ne 0  ]; then
+		local padding=$(( (12 - ${#thisStringHex_array[@]} % 12) ))
+	#otherwise even multiples of 12 still need 12 padding
+	else
+		local padding=12
+	fi	
+
+	#cycle through each element of the array + padding
+	for ((i=0; i < $(( ${#thisStringHex_array[@]} + ${padding})); i++)); do
 		#use modulus to loop through the cipher array elements
 		local charHex_cipher=${cipherHex_array[$(( $i % 11 ))]}
 
 		#get the current hex representation element
 		local charHex=${thisStringHex_array[$i]}
 	
-		#use $(( shell Aritmethic )) to ^ xor the two 0x-prepended hex values to decimal (ugh)
-		#then printf to convert to two char hex value
-		#use xxd to encode to actual value and append to the encodedString variable
-		local encodedString+=$(printf "%02X" "$(( 0x${charHex_cipher} ^ 0x${charHex} ))" | xxd -r -p)
-	done
-
-	#under 12 get padding by subtraction
-	if [ "${#thisStringHex_array[@]}" -lt 12  ]; then
-		local padding=$(( 12 -  ${#thisStringHex_array[@]} ))
-	#over 12 get padding by subtracting remainder of modulo 12
-	elif [ "$(( ${#thisStringHex_array[@]} % 12 ))" -ne 0  ]; then
-		local padding=$(( (12 - ${#thisStringHex_array[@]} % 12) ))
-	#otherwise even multiples of 12 still need 12 padding
-	else
-		local padding=12
-	fi
-	
-	
-	#pad the end so length is multiple of 12
-	local j
-	for ((j=0; j < ${padding}; j++, i++)); do
-		#get current cipher character
-		charHex_cipher=${cipherHex_array[$(( $i % 11 ))]}
-		
-		#add the cipher to the end as padding			
-		encodedString+=$(printf "%02X" "0x${charHex_cipher}" | xxd -r -p)
+		#use $(( shell Aritmethic )) to ^ XOR the two 0x## values (extra padding is 0x00) 
+		#take decimal value and printf convert to two char hex value
+		#use xxd to convert hex to actual value and append to the encodedString variable
+		local encodedString+=$(printf "%02X" "$(( 0x${charHex_cipher} ^ 0x${charHex:-00} ))" | xxd -r -p)
 	done
 
 	#return the string without a newline
