59f01c71f6cab92b8f5caed756223b2bea8fa4cf
Joel Bruner <joel@brunerd.com>
2021-09-16 08:45:28 -0500

    Update getAutoLogin.sh

--- Jamf/scripts/getAutoLogin.sh
+++ Jamf/scripts/getAutoLogin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 : <<-LICENSE_BLOCK
-getAutoLogin (20210913) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
+getAutoLogin (20210916) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
 Licensed under the MIT License
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
