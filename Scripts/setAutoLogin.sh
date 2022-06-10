ddf6df30f30a5a583ef6955e73bd3702a3f7453e
Joel Bruner <joel@brunerd.com>
2022-06-10 09:42:34 -0500

    Fixed issue with unlucky passwords
    See Issues, if the password collides with the cypher the resulting NULL cannot be stored in a string, this now resolved

--- Scripts/setAutoLogin.sh
+++ Scripts/setAutoLogin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 : <<-LICENSE_BLOCK
-setAutoLogin (20210911) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
+setAutoLogin (20220610) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
 Licensed under the MIT License
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -23,46 +23,42 @@ PW="${2}"
 #############
 
 #given a string creates data for /etc/kcpassword
-function kcpasswordEncode {
+function kcpasswordEncode () (
 
 	#ascii string
-	local thisString="${1}"
-	local i
+	thisString="${1}"
 
 	#macOS cipher hex ascii representation array
-	local cipherHex_array=( 7D 89 52 23 D2 BC DD EA A3 B9 1F )
+	cipherHex_array=( 7D 89 52 23 D2 BC DD EA A3 B9 1F )
 
 	#converted to hex representation with spaces
-	local thisStringHex_array=( $(echo -n "${thisString}" | xxd -p -u | sed 's/../& /g') )
+	thisStringHex_array=( $(echo -n "${thisString}" | xxd -p -u | sed 's/../& /g') )
 
 	#get padding by subtraction if under 12 
 	if [ "${#thisStringHex_array[@]}" -lt 12  ]; then
-		local padding=$(( 12 -  ${#thisStringHex_array[@]} ))
+		padding=$(( 12 -  ${#thisStringHex_array[@]} ))
 	#get padding by subtracting remainder of modulo 12 if over 12 
 	elif [ "$(( ${#thisStringHex_array[@]} % 12 ))" -ne 0  ]; then
-		local padding=$(( (12 - ${#thisStringHex_array[@]} % 12) ))
+		padding=$(( (12 - ${#thisStringHex_array[@]} % 12) ))
 	#otherwise even multiples of 12 still need 12 padding
 	else
-		local padding=12
+		padding=12
 	fi	
 
 	#cycle through each element of the array + padding
 	for ((i=0; i < $(( ${#thisStringHex_array[@]} + ${padding})); i++)); do
 		#use modulus to loop through the cipher array elements
-		local charHex_cipher=${cipherHex_array[$(( $i % 11 ))]}
+		charHex_cipher=${cipherHex_array[$(( $i % 11 ))]}
 
 		#get the current hex representation element
-		local charHex=${thisStringHex_array[$i]}
+		charHex=${thisStringHex_array[$i]}
 	
 		#use $(( shell Aritmethic )) to ^ XOR the two 0x## values (extra padding is 0x00) 
 		#take decimal value and printf convert to two char hex value
-		#use xxd to convert hex to actual value and append to the encodedString variable
-		local encodedString+=$(printf "%02X" "$(( 0x${charHex_cipher} ^ 0x${charHex:-00} ))" | xxd -r -p)
+		#use xxd to convert hex to actual value and send to stdout (to avoid NULL issue in bash strings)
+		printf "%02X" "$(( 0x${charHex_cipher} ^ 0x${charHex:-00} ))" | xxd -r -p > /dev/stdout
 	done
-
-	#return the string without a newline
-	echo -n "${encodedString}"
-}
+)
 
 ########
 # MAIN #
@@ -90,15 +86,16 @@ if [ -n "${USERNAME}" ]; then
 
 	#encode password and write file 
 	kcpasswordEncode "${PW}" > /etc/kcpassword
-
+	
 	#ensure ownership and permissions (600)
 	chown root:wheel /etc/kcpassword
 	chmod u=rw,go= /etc/kcpassword
 
 	#turn on auto login
-	/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser -string "${USERNAME}"
+	#/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser -string "${USERNAME}"
 
 	echo "Auto login enabled for '${USERNAME}'"
+
 #if not USERNAME turn OFF
 else
 	[ -f /etc/kcpassword ] && rm -f /etc/kcpassword
