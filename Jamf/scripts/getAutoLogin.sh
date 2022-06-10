0b89db1273cdcb820bad802fab42cd78440e73b8
Joel Bruner <joel@brunerd.com>
2022-06-10 12:02:46 -0500

    Update to handle nulls
    Now properly handles files containing nulls by avoiding variable assignment in bash

--- Jamf/scripts/getAutoLogin.sh
+++ Jamf/scripts/getAutoLogin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 : <<-LICENSE_BLOCK
-getAutoLogin (20210916) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
+getAutoLogin (20210913) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
 Licensed under the MIT License
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -14,38 +14,44 @@ LICENSE_BLOCK
 #############
 
 #given a string from /etc/kcpassword will XOR it back and truncate padding
-function kcpasswordDecode {
+function kcpasswordDecode() (
 
-	#ascii string
-	local thisString="${1}"
-	local i
+	#a string is passed
+	if [ -n "${1}" ]; then
+		#convert to hex representation with spaces
+		thisStringHex_array=( $(echo -n "${1}" | xxd -p -u | sed 's/../& /g') )
+	#file redirection changes the file descriptor
+	elif [ ! -t '0' ]; then
+		#convert to hex representation with spaces
+		thisStringHex_array=( $(xxd -p -u /dev/stdin | sed 's/../& /g') )
+	else
+		echo "No input." >/dev/stderr
+		exit 1
+	fi
 
 	#macOS cipher hex ascii representation array
-	local cipherHex_array=( 7D 89 52 23 D2 BC DD EA A3 B9 1F )
+	cipherHex_array=( 7D 89 52 23 D2 BC DD EA A3 B9 1F )	
 
-	#converted to hex representation with spaces
-	local thisStringHex_array=( $(echo -n "${thisString}" | xxd -p -u | sed 's/../& /g') )
-
-	#cycle through each element of the array + padding
 	for ((i=0; i < ${#thisStringHex_array[@]}; i++)); do
 		#use modulus to loop through the cipher array elements
-		local charHex_cipher=${cipherHex_array[$(( $i % 11 ))]}
+		charHex_cipher=${cipherHex_array[$(( $i % 11 ))]}
 
 		#get the current hex representation element
-		local charHex=${thisStringHex_array[$i]}
+		charHex=${thisStringHex_array[$i]}
+	
+		#use $(( shell Aritmethic )) to ^ XOR the two 0x## values (extra padding is 0x00) 
+		#take decimal value and printf convert to two char hex value
+		#use xxd to convert hex to ascii representation
+		decodedCharacter=$(printf "%02X" "$((0x${charHex_cipher} ^ 0x${charHex:-00}))")		
+		#echo "decodedCharacter: $decodedCharacter"
 
-		#if cipher and character are NOT the same (they also XOR to 00)
-		if [ "${charHex}" != "${charHex_cipher}" ]; then		
-			local encodedString+=$(printf "%02X" "$(( 0x${charHex_cipher} ^ 0x${charHex:-00} ))" | xxd -r -p)
-		else
+		if [[ "${decodedCharacter}" = "00" ]]; then
 			break
+		else
+			printf "%02X" "$(( 0x${charHex_cipher} ^ 0x${charHex:-00} ))" | xxd -r -p > /dev/stdout
 		fi
 	done
-
-	#return the string without a newline
-	echo -n "${encodedString}"
-}
-
+)
 ########
 # MAIN #
 ########
@@ -62,7 +68,7 @@ autoLoginUser=$(/usr/bin/defaults read /Library/Preferences/com.apple.loginwindo
 echo "Auto login user: ${autoLoginUser:-<NOT_SET>}"
 
 if [ -f /etc/kcpassword ]; then
-	echo "Password: $(kcpasswordDecode "$(</etc/kcpassword)")"
+	echo "Password: $(kcpasswordDecode < /etc/kcpassword)"
 else
 	echo "Password: <NOT_SET>"
 fi
