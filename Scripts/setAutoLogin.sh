b8d255f00ee0ef94e87cdc8076af0d4cf1705fb1
Joel Bruner <joel@brunerd.com>
2022-07-31 06:28:50 -0500

    Update setAutoLogin.sh

--- Scripts/setAutoLogin.sh
+++ Scripts/setAutoLogin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 : <<-LICENSE_BLOCK
-setAutoLogin (20220726) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
+setAutoLogin (20220731) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
 Licensed under the MIT License
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -31,7 +31,7 @@ function kcpasswordEncode () (
 	cipherHex_array=( 7D 89 52 23 D2 BC DD EA A3 B9 1F )
 
 	#converted to hex representation with spaces
-	thisStringHex_array=( $(echo -n "${thisString}" | xxd -p -u | sed 's/../& /g') )
+	thisStringHex_array=( $(/bin/echo -n "${thisString}" | xxd -p -u | sed 's/../& /g') )
 
 	#get padding by subtraction if under 12 
 	if [ "${#thisStringHex_array[@]}" -lt 12  ]; then
