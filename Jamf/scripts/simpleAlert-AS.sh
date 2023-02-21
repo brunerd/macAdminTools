67ca6a3fcaa4320259e4b7381b7a736dcc6f2403
Joel Bruner <joel@brunerd.com>
2023-02-21 11:43:36 -0600

    Update simpleAlert-AS.sh

--- Jamf/scripts/simpleAlert-AS.sh
+++ Jamf/scripts/simpleAlert-AS.sh
@@ -9,13 +9,22 @@
 #function to interpret the escapes and fixup characters that can screw up Applescript if unescaped \ and "
 function interpretEscapesFixBackslashesAndQuotes()(echo -e "${@}" | sed -e 's/\\/\\\\/g' -e 's/"/\\"/g')
 
-#function interpretEscapesFixBackslashesAndQuotes()(echo -e "$(sed -e 's/\\/\\\\/g' <<< "${@}")" | sed -e 's/\\/\\\\/g' -e 's/"/\\"/g')
+function jamflog(){
+	local logFile="/var/log/jamf.log"
+	#if we cannot write to the log or it does not exist, unset and tee simply echoes
+	[ ! -w "${logFile}" ] && unset logFile
+	#this will tee to jamf.log in the jamf log format: <Day> <Month> DD HH:MM:SS <Computer Name> ProcessName[PID]: <Message>
+	echo "$(date +'%a %b %d %H:%M:%S') ${myComputerName:="$(scutil --get ComputerName)"} ${myName:="$(basename "${0}" | sed 's/\..*$//')"}[${myPID:=$$}]: ${1}" | tee -a "${logFile}" 2>/dev/null
+}
 
-#fix up our inputs
+
+#process our input then escape for AppleScript
 message=$(interpretEscapesFixBackslashesAndQuotes "${4}")
 title=$(interpretEscapesFixBackslashesAndQuotes "${5}")
-#could be a path or a built-in (stop, caution, note)
+#could be a path or a built-in icon (stop, caution, note)
 icon="${6}"
+#invoke the system open command with this argument (URL, preference pane, etc...)
+open_item="${7}"
 
 #these are the plain icons (Applescript otherwise badges them with the calling app)
 case "${icon}" in
@@ -31,11 +40,19 @@ if [ -f "${icon}" ]; then
 	withIcon_AS="with icon file (POSIX file \"${icon}\")"
 fi
 
+jamflog "Prompting user: $(stat -f %Su /dev/console)"
+
+#prompt the user, giving up and moving on after 1 day (86400 seconds)
 /usr/bin/osascript <<-EOF
 activate
-with timeout of 133200 seconds
-	display dialog "${message}" with title "${title}" ${withIcon_AS} buttons {"OK"} default button "OK"
+with timeout of 86400 seconds
+	display dialog "${message}" with title "${title}" ${withIcon_AS} buttons {"OK"} default button "OK" giving up after "86400"
 end timeout
 EOF
+
+if [ -n "${open_item}" ]; then
+	jamflog "Opening: ${open_item}"
+	open "${open_item}"
+fi
 
 exit 0
