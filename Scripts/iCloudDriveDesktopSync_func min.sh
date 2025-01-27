a1dd5e825c4eb22964402c532adf8b95203bc89a
Joel Bruner <joel@brunerd.com>
2025-01-27 16:40:03 -0600

    Update iCloudDriveDesktopSync_func min.sh

--- Scripts/iCloudDriveDesktopSync_func min.sh
+++ Scripts/iCloudDriveDesktopSync_func min.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #Joel Bruner (repo: https://github.com/brunerd, blog: https://brunerd.com/blog)
-#iCloudDriveDesktopSync (min) - gets the iCloud Drive Desktop and Document Sync Status for the console user
+#iCloudDriveDesktopSync (min) - gets the iCloud Drive Desktop and Document Sync Status for the console user (updated with new macOS Sequoia attribute also)
 
 #############
 # FUNCTIONS #
 #############
 
 #here's your minified function
-function iCloudDriveDesktopSync()(consoleUser=$(stat -f %Su /dev/console);if [ "${consoleUser}" = "root" ]; then consoleUser=$(/usr/bin/last -1 -t console | awk '{print $1}');fi;xattr_desktop=$(sudo -u $consoleUser /bin/sh -c 'xattr -p com.apple.icloud.desktop ~/Desktop 2>/dev/null');if [ -z "${xattr_desktop}" ]; then return 1;else return 0;fi)
+function iCloudDriveDesktopSync()( consoleUser=$(stat -f %Su /dev/console);[ "${consoleUser}" = "root" ] && consoleUser=$(/usr/bin/last -1 -t console | awk '{print $1}');homeFolder=$(/usr/libexec/PlistBuddy -c "print dsAttrTypeStandard\:NFSHomeDirectory:0" /dev/stdin 2>/dev/null <<< "$(dscl -plist . -read "/Users/${consoleUser}" NFSHomeDirectory)");grep -q -E 'com\.apple\.file-provider-domain-id|com\.apple\.icloud\.desktop' <<< $(xattr "${homeFolder}/Desktop") && return 0 || return 1; )
 
 #example function usage, if leverages the return values
 if iCloudDriveDesktopSync; then
