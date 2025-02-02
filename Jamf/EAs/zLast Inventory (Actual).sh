d877a390e34a83f3351e8cdf91cf7cfd4641d17b
Joel Bruner <joel@brunerd.com>
2025-02-02 00:19:50 -0600

    Update zLast Inventory (Actual).sh

--- Jamf/EAs/zLast Inventory (Actual).sh
+++ Jamf/EAs/zLast Inventory (Actual).sh
@@ -3,12 +3,12 @@
 # A Jamf Pro Extension Attribute to report the _actual time_ of a successful inventory submission
 # This is to address two issues with the built-in Last Inventory:
 #  1) This date stamp is not affected by API writes to the computer record
-#  2) Detect "Unknown Error" inventory submission failures if date does not match built-in this indicates failure
+#  2) Detect "Unknown Error" inventory submission failures if date does not match built-in Last Inventory date (excluding API writes in History)
 
 # Notes:
-# The "z" at the beginning of the EA name is to ensure it is run last and is closest to built-in Last Inventory datestamp
-# EA date types do not consider offsets so we must normalize the time to UTC (-u) for the most consistent behavior
-# When viewing the date in Reports, Inventory Display, or Computer Records it will not be localized like built-in
+# The "z" at the beginning of this EA's name is to ensure it is runs last and is closest to built-in Last Inventory datestamp
+# The Jamf EA date type does not consider time zone offsets so we must normalize the time to UTC (-u) for the most consistent behavior
+# When viewing an EA date in a Report, Inventory Display, or Computer Record it will not be localized according to Preferences as built-in dates are
 
 #ISO 8601(-ish) date format with UTC/GMT normalized time (YYYY-MM-DD HH:MM:SS)
 DATE_NORMALIZED=$(date -u +"%F %T")
